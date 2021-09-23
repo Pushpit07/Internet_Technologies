095792203b019ae67d3d77efb84d2d8fa36550db
Pushpit <pushpit07@gmail.com>
2021-09-23 13:41:49 +0530

    Tables
    Signed-off-by: Pushpit <pushpit07@gmail.com>

---
+++ 10/main.js
@@ -0,0 +1,39 @@
+var current = 2;
+
+function generateRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var randomColor = "#";
+
+	for (var j = 0; j < 6; j++) {
+		randomColor += letters[Math.floor(Math.random() * 16)];
+	}
+	return randomColor;
+}
+
+function printTables() {
+	var table = "";
+
+	table +=
+		"<div style='color: " +
+		generateRandomColor() +
+		"; font-size:" +
+		(20 + current) +
+		"px'>" +
+		" Table of " +
+		current +
+		" : ";
+
+	for (var i = 1; i <= 10; i++) {
+		table = table + "<span>" + current * i + " " + "</span>";
+	}
+
+	table += "</div>";
+	current++;
+
+	if (current === 11) {
+		clearInterval(inter);
+	}
+	document.getElementById("result1").innerHTML += table;
+}
+
+var inter = setInterval(printTables, 1500);
